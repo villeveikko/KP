dc067cdf073c5bfd77a3ae291cfa6b3445af7c22
Villeveikko Sula <villeveikkosula@hotmail.com>
2018-02-24 17:51:13 +0200

    Mahticommit

---
+++ Kaukovuorten pakolaiset - Visual novel/Assets/Scripts/VaazunMonistaja.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VaazunMonistaja : MonoBehaviour {
+
+    public Transform prefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void nappulaPainettu()
+    {
+        Transform apu = Instantiate<Transform>(prefab);
+        apu.SetPositionAndRotation(new Vector3(-1f, -1f, 0), Quaternion.identity);
+    }
+}
